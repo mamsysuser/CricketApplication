8a4c6129073c58652e351e9f45f0773c7e99f7e1
mamsysuser <sunil.gaur@mamsys.com>
2019-08-12 14:15:53 +0530

    Authentication and Login Test to application

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -8,13 +8,23 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LoginTest extends TestCase
 {
+    use RefreshDatabase;
     /**
-     * A basic feature test example.
+     * Admin Login Test by Sunil Gaur.
      *
      * @return void
      */
     public function testAdminLogin()
     {
         $response = $this->get('/admin')->assertRedirect('/login');
+    }
+
+    
+    // Authentication Test
+    public function testAuthenticatedUser()
+    {
+        $this->actingAs(factory(User::class)->create());
+        $response = $this->get('/admin')
+        ->assertOk();
     }
 }
